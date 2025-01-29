310420f84509d245719ba990b29041c9725c1b03
dylan202433 <dylanandersonlauu@gmail.com>
2025-01-29 19:06:45 +0700

    Add files via upload

---
+++ config/nwg-dock-hyprland/launch.sh
@@ -0,0 +1,9 @@
+#    ___           __  
+#   / _ \___  ____/ /__
+#  / // / _ \/ __/  '_/
+# /____/\___/\__/_/\_\ 
+#                      
+
+if [ -f $HOME/.config/ml4w/settings/nwg-dock-hyprland.sh ] ;then
+    nwg-dock-hyprland -i 20 -w 5 -mb 10 -ml 10 -mr 10 -x 
+fi
